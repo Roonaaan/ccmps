c775035d79c945a41047245ee43af1cc1c892cd7
Roonaaan <rjohnronan2001@gmail.com>
2024-03-18 18:46:32 +0800

    Revert Changes

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
   app.use(express.json());
   app.use(
     cors({
-      origin: "https://ccmps.vercel.app",
+      origin: "http://localhost:5173",
     })
   );
   app.use(cookieParser());
