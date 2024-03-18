4a6c8271c3bf1051ebbf2b51d6ff494a3f45ed4e
Roonaaan <rjohnronan2001@gmail.com>
2024-03-18 18:44:27 +0800

    Testing Login - Attempt 2

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
   app.use(express.json());
   app.use(
     cors({
-      origin: "https://ccmps.vercel.app/",
+      origin: "https://ccmps.vercel.app",
     })
   );
   app.use(cookieParser());
