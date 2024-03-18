e53a2bd5b8a3c3a3ce61d7a76aa6d170ab5fd3af
Roonaaan <rjohnronan2001@gmail.com>
2024-03-19 01:30:45 +0800

    Changed Backend Origin Link

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://ccmps.netlify.app/",
     })
 );
 app.use(cookieParser());
