78c35f675e10f037e5d96c848387ac1b2572c8b6
Roonaaan <rjohnronan2001@gmail.com>
2024-03-20 00:40:42 +0800

    Database. js - Minor Changes fix

--- backend/database.js
+++ backend/database.js
@@ -1,32 +1,8 @@
 import mysql from "mysql";
 
-// Create a MySQL connection pool
-const pool = mysql.createPool({
-  connectionLimit: 10, // Adjust this value as needed
+export const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "", // Enter your MySQL password here
+  password: "",
   database: "ccdb",
 });
-
-// Function to execute SQL queries
-export const executeQuery = (query, params) => {
-  return new Promise((resolve, reject) => {
-    // Get a connection from the pool
-    pool.getConnection((err, connection) => {
-      if (err) {
-        return reject(err);
-      }
-      // Execute the query with the provided parameters
-      connection.query(query, params, (err, results) => {
-        // Release the connection back to the pool
-        connection.release();
-        if (err) {
-          return reject(err);
-        }
-        // Resolve with the query results
-        resolve(results);
-      });
-    });
-  });
-};
