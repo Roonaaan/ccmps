750b79a07fa1a3bd46fe61230ed190cc35bc19c4
Roonaaan <rjohnronan2001@gmail.com>
2024-03-19 13:28:57 +0800

    Hardcoded Backend

--- backend/database.js
+++ backend/database.js
@@ -1,12 +1,8 @@
-import dotenv from "dotenv";
 import mysql from "mysql";
 
-dotenv.config();
-
 export const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT,
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "ccdb",
 });
