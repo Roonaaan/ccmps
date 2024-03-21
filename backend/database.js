06832582a5b5f2280c832426b3c7d3cdbaf61b34
Roonaaan <rjohnronan2001@gmail.com>
2024-03-22 05:35:38 +0800

    adjusted database.js

--- backend/database.js
+++ backend/database.js
@@ -3,7 +3,7 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL,
+  connectionString: "postgres://default:NpLQ8gFc1dsD@ep-aged-meadow-a1op3qk0-pooler.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require",
 });
 
 (async () => {
