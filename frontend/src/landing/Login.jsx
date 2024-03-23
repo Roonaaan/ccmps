e9c16fdbd5b41813e21531263eec87870dfd3fd8
Roonaaan <rjohnronan2001@gmail.com>
2024-03-23 17:22:13 +0800

    User Profile and Testing Environment Minor Bugs Fixed

--- frontend/src/landing/Login.jsx
+++ frontend/src/landing/Login.jsx
@@ -66,7 +66,8 @@ export const Login = ({ onClose }) => {
             await new Promise(resolve => setTimeout(resolve, 1000));
             const response = await axios.post('http://localhost:8800/api/auth/login', { email, password }, { withCredentials: true });
             if (response.data) {
-                sessionStorage.setItem('token', response.data.token);
+                sessionStorage.setItem('user', email);
+                {/* sessionStorage.setItem('token', response.data.token); */}
                 setSuccessMsg('Welcome');
                 setSuccessMsg(<span style={{ color: 'green' }}> Welcome </span>);
                 setTimeout(() => {
