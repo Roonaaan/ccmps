c0dc274b5bd4a89267f6b085dbc0bc03c0e10008
Roonaaan <rjohnronan2001@gmail.com>
2024-03-24 02:36:29 +0800

    Python Backend - Minors Bugs fix

--- frontend/src/signed_in/Recommend/Recommend.jsx
+++ frontend/src/signed_in/Recommend/Recommend.jsx
@@ -48,7 +48,6 @@ const Recommend = () => {
         const fetchRecommendations = async () => {
             try {
                 const userEmail = sessionStorage.getItem('user');
-                const response = await fetch(`http:localhost:5000/recommend?email=${userEmail}`);
                 const response = await fetch(`https://ccmps-python.onrender.com/recommend?email=${userEmail}`);
                 const data = await response.json();
                 console.log('Fetched recommendations:', data); // Log the fetched data
