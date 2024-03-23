c6543a73233413aac5720c75e4a9f68e79ebddea
Roonaaan <rjohnronan2001@gmail.com>
2024-03-23 10:18:28 +0800

    Fullstack Changes - CORS Error

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(
     cors({
-        origin: "https://localhost:5173",
+        origin: "http://localhost:5173",
     })
 );
 app.use(cookieParser());
