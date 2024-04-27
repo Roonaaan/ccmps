4e1f6b23322968a1d4d13a970a0d2153678d3b83
iCodeAzure <bulawan.christoper.bscs2020@gmail.com>
2024-04-28 01:11:18 +0800

    admin-dashboard-updates

--- frontend/src/admin/dashboard/HomeDashboard.jsx
+++ frontend/src/admin/dashboard/HomeDashboard.jsx
@@ -7,7 +7,39 @@ function HomeDashboard() {
       <div className='home'>
         <div className='home-header'>
           <h1>Welcome Admin!</h1>
+          <p>Dashboard</p>
         </div>
+
+      <div className='stats-parent'>
+        <div className='upper-row-stats'>
+          <div className='row-box'>1</div>
+          <div className='row-box'>2</div>
+          <div className='row-box'>3</div>
+          <div className='row-box'>4</div>
+        </div>
+
+        <div className='middle-row-graphs'>
+          <div className='graph-box'>1</div>
+          <div className='graph-box'>2</div>
+
+        </div>
+
+        <div className='lower-row-stats'>
+          <div className='low-row-box'>1</div>
+          <div className='low-row-box'>2</div>
+          <div className='low-row-box'>3</div>
+          <div className='low-row-box'>4</div>
+        </div>
+
+        <div className='statistic-group'>
+          <div className='statistic-chuchu'>1</div>
+          <div className='statistic-chuchu'>2</div>
+          <div className='statistic-chuchu'>3</div>
+          
+        </div>
+        </div>
+        
+        {/* 
         <div className='home-table-row'>
           <div className='home-type-employee'>
             <p>Number of Employee</p>
@@ -16,6 +48,8 @@ function HomeDashboard() {
             <p>Roadmap Created</p>
           </div>
         </div>
+
+        
         <div className='home-graph-row'>
           <div className="home-graph-employee">
             <div className="home-graph-card">
@@ -32,6 +66,7 @@ function HomeDashboard() {
             </div>
           </div>
         </div>
+        */}
       </div>
     </div>
   );
