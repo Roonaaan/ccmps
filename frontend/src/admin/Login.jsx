cd88539efec374a356103a0f39906907136b97ea
iCodeAzure <bulawan.christoper.bscs2020@gmail.com>
2024-05-04 11:50:20 +0800

    admin ui

--- frontend/src/admin/Login.jsx
+++ frontend/src/admin/Login.jsx
@@ -1,80 +1,88 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 // CSS and Assets
-import Logo from '../assets/login/logo-light.png';
-import './styles/Login.css';
+import Logo from "../assets/login/logo-light.png";
+import "./styles/Login.css";
 
 function Login() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [errorMessage, setErrorMessage] = useState('');
-    const navigate = useNavigate(); // Using useNavigate for navigation
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate(); // Using useNavigate for navigation
 
-    const handleLogin = async () => {
-        try {
-            const response = await fetch('http://localhost:8800/api/auth/admin-login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ email, password }),
-            });
-
-            if (!response.ok) {
-                const errorData = await response.json();
-                setErrorMessage(errorData.message || 'Failed to login');
-            } else {
-                const userData = await response.json();
-                sessionStorage.setItem('email', email);
-                sessionStorage.setItem('role', userData.role); // Store role in session storage
-                // Redirect to welcome page after successful login
-                navigate('/Admin/Welcome');
-            }
-        } catch (error) {
-            console.error('Login error:', error);
-            setErrorMessage('Failed to login. Please try again later.');
+  const handleLogin = async () => {
+    try {
+      const response = await fetch(
+        "http://localhost:8800/api/auth/admin-login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, password }),
         }
-    };
+      );
 
-    return (
-        <div className="adminContainer">
-            <div className="adminPanel">
-                <div className="adminLogoPanel">
-                    <img src={Logo} alt="Logo" className="adminLogo" />
-                </div>
-                <div className="adminLoginPanel">
-                    <div className="adminLoginPanelHeader">
-                        <h1>Admin Login</h1>
-                    </div>
-                    <div className="adminLoginPanelInputs">
-                        <div className="adminLoginPanelInput">
-                            <input
-                                type="email"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                            />
-                            <label htmlFor="email">Email Address</label>
-                        </div>
-                        <div className="adminLoginPanelInput">
-                            <input
-                                type="password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                            />
-                            <label htmlFor="password">Password</label>
-                        </div>
-                    </div>
-                    <div className="adminLoginPanelLogin">
-                        <button className="adminLoginButton" onClick={handleLogin}>
-                            Log In
-                        </button>
-                        {errorMessage && <p className="errorMessage">{errorMessage}</p>}
-                    </div>
-                </div>
-            </div>
+      if (!response.ok) {
+        const errorData = await response.json();
+        setErrorMessage(errorData.message || "Failed to login");
+      } else {
+        const userData = await response.json();
+        sessionStorage.setItem("email", email);
+        sessionStorage.setItem("role", userData.role); // Store role in session storage
+        // Redirect to welcome page after successful login
+        navigate("/Admin/Welcome");
+      }
+    } catch (error) {
+      console.error("Login error:", error);
+      setErrorMessage("Failed to login. Please try again later.");
+    }
+  };
+
+  return (
+    <div className="adminContainer">
+      <div className="adminPanel">
+        {/* 
+        <div className="adminLogoPanel">
+          <img src={Logo} alt="Logo" className="adminLogo" /> 
         </div>
-    );
+        */}
+        <div className="adminLoginPanel">
+          <div className="adminLoginPanelHeader">
+            <h1>ADMIN LOGIN</h1>
+          </div>
+          <div className="adminLoginPanelInputs">
+            <div className="admin-panel-inp">
+              <label htmlFor="email">Email Address</label>
+              <input
+                className="input-box"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+
+            <div className="admin-panel-inp">
+              <label htmlFor="password">Password</label>
+              <input
+                className="input-box"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="adminLoginPanelLogin">
+            <button className="adminLoginButton" onClick={handleLogin}>
+              Log In
+            </button>
+            {errorMessage && <p className="errorMessage">{errorMessage}</p>}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
